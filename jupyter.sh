945a2a87ba82662dc41f50bc2725d8f8ca2518c8
Guangzeng Han <77609872+CUCHon@users.noreply.github.com>
2025-04-15 15:20:41 -0500

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -5,11 +5,11 @@
 #SBATCH --partition=bigTiger
 #SBATCH --gres=gpu:1
 #SBATCH --time=0-2:00:00
-#SBATCH --nodelist=itiger06
+#SBATCH --nodelist=itiger06  #itiger01-11 
 #SBATCH --job-name=jupyter
 
-
-PORT=8888
+#give PORT a 4_digits_number
+PORT= a_4_digits_number
 
 echo "*** Starting Jupyter on: "$(hostname)" at port "$PORT
 
